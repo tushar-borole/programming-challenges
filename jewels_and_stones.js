30ee3708c18778a2cdae1c89cc4dedd23f5e8e23
Tushar  Borole <tushariscoolster@gmail.com>
2018-09-13 20:09:40 -0700

    jewels and stones added

---
+++ jewels_and_stones.js
@@ -0,0 +1,37 @@
+// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+//
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+//
+//     Example 1:
+//
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+// Example 2:
+//
+// Input: J = "z", S = "ZZ"
+// Output: 0
+// Note:
+//
+//     S and J will consist of letters and have length at most 50.
+// The characters in J are distinct.
+// https://leetcode.com/problems/jewels-and-stones/description/
+
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    const arrayJ=J.split("")
+    const arrayS=S.split("")
+    let count=0
+    for(i=0;i<arrayJ.length;i++){
+        for(j=0;j<arrayS.length;j++){
+            if(arrayJ[i]==arrayS[j]){
+                count++
+            }
+        }
+    }
+
+    return count
+};
